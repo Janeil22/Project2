3b9c622b006e7ae2f30d92136a14005aa1a16757
Jesuloba David <jesulobadavid@gmail.com>
2022-07-07 03:39:22 +0000

    latest update

--- project.py
+++ project.py
@@ -1,15 +1,42 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
+#root URL to derive events
 url = "https://app.ticketmaster.com/discovery/v2/events"
 
 API_KEY = "zWmwA15ShfzkNwMGKQ7Ih2RDbAWaoIvV"
 
+#Event Search Parameters
 parameters = {'apikey':API_KEY, 'countryCode':'US', 'classificationName':'music', 'startDateTime':'2022-07-01T14:00:00Z', 'endDateTime': '2022-08-01T14:00:00Z', 'sort':'date,asc', 'city':'', 'stateCode':''}
 
+#Get City name and State Code from user
 parameters['city'] = "Las Vegas" #input("Enter City: ")
 parameters['stateCode'] = "NV" #input("Enter State Code: ")
 
+#get all events within city
 response = requests.get(url, parameters)
 response = response.json()
+Events = list()
+
+#extract Event Id from json and input in empty list
+for event in response:
+    for x in (response["_embedded"]["events"]):
+        Events.append(x["id"])
+
+#Events = {"EventIDs":Events, "location":[], "Date":[], "Performer" : []}
 
 
+details = requests.get('https://app.ticketmaster.com/discovery/v2/events/' +Events[0]+ '.json?apikey=zWmwA15ShfzkNwMGKQ7Ih2RDbAWaoIvV', {'locale':'en-us'})
+print(details.json())
+
+'''for event in Events["EventIDs"]:
+    details = requests.get(url, {'id':event, 'apikey':API_KEY})
+    details = details.json()
+    print(details)
+    break
+    for x in (details["_embedded"]["venues"]):
+        Events["location"].append(["name"])
+        Events["location"].append(["address"]["line1"])
+        Events["location"].append(["city"]["name"])
+        Events["location"].append(["state"]["stateCode"])'''
