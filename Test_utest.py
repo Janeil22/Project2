58cfdf46862515f26e1266353bd3651fa8debf76
Jesuloba David <jesulobadavid@gmail.com>
2022-07-08 13:07:29 +0000

    Edited Test

--- Test_utest.py
+++ Test_utest.py
@@ -1,14 +1,13 @@
 import unittest
 from Project import get_city, get_state
 
-class Test_utest(unittest.TestCase):
 
-    def test_city(self): 
+class Test_utest(unittest.TestCase):
+    def test_city(self):
         self.assertEqual('las vegas', 'las vegas')
 
     def test_iscity():
         self.assertTrue('las vegas')
-
 
     def test_state(self):
         self.assertEqual('nv', 'nv')
@@ -17,7 +16,5 @@ class Test_utest(unittest.TestCase):
         self.assertTrue('nv')
 
 
-
 if __name__ == '__main__':
     unittest.main()
-   
