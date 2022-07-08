e784ee35baa6d854cf0f59254674d35e4e9bf1de
Janeil Speid <speidjaneil@gmail.com>
2022-07-08 04:19:01 +0000

    Final with rough unittest

---
+++ Test_utest.py
@@ -0,0 +1,23 @@
+import unittest
+from Project import get_city, get_state
+
+class Test_utest(unittest.TestCase):
+
+    def test_city(self): 
+        self.assertEqual('las vegas', 'las vegas')
+
+    def test_iscity():
+        self.assertTrue('las vegas')
+
+
+    def test_state(self):
+        self.assertEqual('nv', 'nv')
+
+    def test_isstate():
+        self.assertTrue('nv')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+   
